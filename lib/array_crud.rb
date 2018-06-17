18572f00766e2f268f4a791b106dee14198dfbed
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-17 17:30:11 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
 
-  add_element_to_end_of_array(array, element) << "arrays!"
+  add_element_to_end_of_array << "arrays!"
   puts add_element_to_end_of_array.inspect
 end
 
